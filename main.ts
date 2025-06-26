eefa610e11c0d9670b451b86e5cea16ce9780de0
UIO辛可 <2692571183@qq.com>
2025-06-26 14:56:11 +0800

    4修改仓库名字内容1

--- main.ts
+++ main.ts
@@ -1,17 +1,11 @@
 /**
-* Functions to micro:bit XGO Robot Kit by ELECFREAKS Co.,Ltd.
-*/
-//% color=#8600FF icon="\uf1b0"
-//% block="xgo" blockId="xgo"
-namespace xgo {
-
-    export enum rotate_enum {
-        //% block="Left"
-        Left,
-        //% block="Right"
-        Right
-    }
-
+ * This extension is designed to programme and drive the Smart AI Lens(二郎神)
+ */
+//% color=#0031AF icon="\uf06e" 
+//% groups='["Basic", "Ball", "Face", "Card", "Color", "Tracking", "Learn"]'
+//% block="PlanetX_AI-Lens"
+namespace PlanetX_AILens {
+ 
     export enum FuncList {
         //% block="Card recognition"
         Card = 2,
@@ -25,33 +19,6 @@ namespace xgo {
         Color = 9,
         //% block="Learn Object"
         Things = 10
-    }
-    //% block="rotate %direction,speed is %speed\\%"
-    //% speed.min=0 speed.max=100
-    export function rotate(direction: rotate_enum, speed: number) {
-        let rotate_buffer = pins.createBuffer(9)
-        rotate_buffer[0] = 0x55
-        rotate_buffer[1] = 0x00
-        rotate_buffer[2] = 0x09
-        rotate_buffer[3] = 0x00
-        rotate_buffer[4] = 0x32
-        rotate_buffer[7] = 0x00
-        rotate_buffer[8] = 0xAA
-        if (speed > 100)
-            speed = 100
-        if (speed < 0)
-            speed = 0
-        switch (direction) {
-            case rotate_enum.Right:
-                rotate_buffer[5] = Math.map(speed, 0, 100, 128, 0)
-                rotate_buffer[6] = ~(0x09 + 0x00 + 0x32 + rotate_buffer[5])
-                break
-            case rotate_enum.Left:
-                rotate_buffer[5] = Math.map(speed, 0, 100, 128, 255)
-                rotate_buffer[6] = ~(0x09 + 0x00 + 0x32 + rotate_buffer[5])
-                break
-        }
-        serial.writeBuffer(rotate_buffer)
     }
 
     //% block="Initialize AI-Lens"
